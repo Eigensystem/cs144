1220960d067982df11ce03b901312d802a0ccab2
Eigensystem <eigensystem1318@gmail.com>
2022-12-01 16:35:47 +0800

    finished: week2-wrapper & unwrapper

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-
+#include <iostream>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t num = static_cast<uint32_t>(n << 32 >> 32);
+    return isn + num;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,15 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t wrapped_abs = n - isn;
+    // cout << "\nn:" << n <<" isn:" << isn << " num:" << num << endl;
+    uint32_t wrapped_chk = checkpoint << 32 >> 32;
+    int32_t off = wrapped_abs - wrapped_chk;
+    // cout << checkpoint << " " << off << endl;
+    if (checkpoint <= INT32_MAX) {
+        return checkpoint + static_cast<uint32_t>(off);
+    }
+    else {
+        return checkpoint + off;
+    }
 }
